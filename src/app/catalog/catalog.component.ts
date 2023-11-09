3133886daf809660341c5a2b6f8134bf8e559484
Shahrose <shahrose.khan@lyftyfy.com>
2023-11-09 10:52:01 +0500

    Added: Main Routing and Data from API

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product.model';
-import { CartService } from '../cart.service';
+import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -12,22 +13,28 @@ export class CatalogComponent {
   products: any;
   filter: string = '';
 
-  constructor(private cartSvc: CartService, private productSvc: ProductService) {
-  }
+  constructor(
+    private cartSvc: CartService,
+    private productSvc: ProductService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.productSvc.getProducts().subscribe((products => {
+    this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
-    }))
+    });
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 
   getFilteredProducts() {
     return this.filter === ''
       ? this.products
-      : this.products.filter((product:any) => product.category === this.filter);
+      : this.products.filter(
+          (product: any) => product.category === this.filter
+        );
   }
 }
